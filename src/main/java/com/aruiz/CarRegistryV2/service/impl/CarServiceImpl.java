aa3fde44c58c68e6d8d2a53da9340eb5495a01b0
Antonio Ruiz Benito <3speedemon3@gmail.com>
2025-06-19 20:58:07 +0200

    Add new methods in service

--- src/main/java/com/aruiz/CarRegistryV2/service/impl/CarServiceImpl.java
+++ src/main/java/com/aruiz/CarRegistryV2/service/impl/CarServiceImpl.java
@@ -2,13 +2,16 @@ package com.aruiz.CarRegistryV2.service.impl;
 
 import com.aruiz.CarRegistryV2.controller.dto.CarRequest;
 import com.aruiz.CarRegistryV2.controller.dto.CarResponse;
+import com.aruiz.CarRegistryV2.entity.CarEntity;
 import com.aruiz.CarRegistryV2.repository.CarRepository;
 import com.aruiz.CarRegistryV2.service.CarService;
+import com.aruiz.CarRegistryV2.service.converter.CarConverter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Slf4j
@@ -17,13 +20,34 @@ public class CarServiceImpl implements CarService {
     @Autowired
     private CarRepository repository;
 
+    @Autowired
+    private CarConverter converter;
+
     @Override
     public CarResponse save(CarRequest car) throws Exception {
+
+        if (car != null) {
+            CarEntity entity = converter.toCarEntity(car);
+            repository.save(entity);
+            log.info("Car save");
+            return converter.toCarResponse(entity);
+        }
+
         return null;
     }
 
     @Override
     public CarResponse findById(Long id) throws Exception {
+
+        Optional<CarEntity> entity = repository.findById(id);
+
+        if (entity.isPresent()) {
+
+            CarResponse response = converter.toCarResponse(entity.get());
+            log.info("Car find");
+            return response;
+        }
+
         return null;
     }
 
@@ -39,6 +63,13 @@ public class CarServiceImpl implements CarService {
 
     @Override
     public boolean removeById(Long id) throws Exception {
+
+        if (repository.findById(id).isPresent()) {
+            repository.deleteById(id);
+            log.info("Car delete");
+            return true;
+        }
+
         return false;
     }
 }
